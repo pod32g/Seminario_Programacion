274f4351f29ba8db888a23dff6eb310f205ae222
pod32g <pod32g@pod32g.net>
2015-10-01 11:56:10 -0500

    Fixed multi platform support

--- pre.h
+++
@@ -1,7 +0,0 @@
-#ifdef _WIN32 || _WIN64
-	#define CLEAR std::system("cls");
-	#define PAUSE std::system("pause");
-#else 
-    #define CLEAR std::system("clear");
-	#define PAUSE std::system("sleep 5s");
-#endif
